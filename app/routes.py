ea6419b6e18ff3b3d2d8d933ff41882ebd558920
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-04-17 17:43:58 -0400

    Add edit functionality and view

--- app/routes.py
+++ app/routes.py
@@ -15,8 +15,8 @@ def index():
     return render_template("index.html", tunes=tunes, form=form)
 
 
-@app.route('/entry', methods=['GET', 'POST'])
-def entry():
+@app.route('/tune_entry', methods=['GET', 'POST'])
+def tune_entry():
     form = TuneForm()
     if form.validate_on_submit():
         print("Validated.")
@@ -56,6 +56,53 @@ def entry():
         )
         db.session.add(new_tune)
         db.session.commit()
-        flash(f"Tune {new_tune.title} entered.", 'info')
+        flash(f"Tune {new_tune.id}, \"{new_tune.title}\", entered.", 'info')
         return redirect(url_for('entry'))
     return render_template("entry.html", form=form)
+
+@app.route("/edit_tune/<tune_id>", methods=["GET", "post"])
+def edit_tune(tune_id):
+    form = TuneForm()
+    tune_to_edit = Tune.query.get_or_404(tune_id)
+    if form.validate_on_submit():
+        tune_to_edit.title = form.title.data
+        tune_to_edit.composer = form.composer.data
+        tune_to_edit.key = form.key.data
+        tune_to_edit.other_key = form.other_key.data
+        tune_to_edit.song_form = form.song_form.data
+        tune_to_edit.style = form.style.data
+        tune_to_edit.meter = form.meter.data
+        tune_to_edit.year = form.year.data
+        tune_to_edit.decade = form.decade.data
+        tune_to_edit.knowledge = form.knowledge.data
+        db.session.commit()
+        flash("Changes saved.", "info")
+        return redirect(url_for("index"))
+
+    form.title.data = tune_to_edit.title
+    form.composer.data = tune_to_edit.composer
+    form.key.data = tune_to_edit.key
+    form.other_key.data = tune_to_edit.other_key
+    form.song_form.data = tune_to_edit.song_form
+    form.style.data = tune_to_edit.style
+    form.meter.data = tune_to_edit.meter
+    form.year.data = tune_to_edit.year
+    form.decade.data = tune_to_edit.decade
+    form.knowledge.data = tune_to_edit.knowledge
+    return render_template("edit.html", form=form, tune=tune_to_edit)
+
+
+@app.route("/delete/<entry_id>")
+def delete_entry(entry_id):
+    entry_to_delete = Entry.query.get_or_404(entry_id)
+    if entry_to_delete.submitter != current_user:
+        flash("You are not authorized to delete this entry.", "dark")
+        return redirect(url_for("index"))
+
+    db.session.delete(entry_to_delete)
+    db.session.commit()
+    flash(
+        'Entry deleted.',
+        "info",
+    )
+    return redirect(url_for("index"))
